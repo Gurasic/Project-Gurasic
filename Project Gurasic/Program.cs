7bab2d059dfea9d8b33e37b01aaf5d4de46eab39
Gurasic <gurasic1234@gmail.com>
2023-09-03 18:09:40 +0200

    Agregar archivos de proyecto.

---
+++ Project Gurasic/Program.cs
@@ -0,0 +1,10 @@
+﻿Settings.WindowTitle = "My SadConsole Game";
+
+Game.Configuration gameStartup = new Game.Configuration()
+    .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+    .SetStartingScreen<Project_Gurasic.Scenes.RootScene>()
+    ;
+
+Game.Create(gameStartup);
+Game.Instance.Run();
+Game.Instance.Dispose();
